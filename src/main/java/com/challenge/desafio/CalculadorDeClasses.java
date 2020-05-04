4d36dbd2b23563311e4b54061c5c76c2dad03ef3
Rodrigo Prates <rodrigo.prates@yaman.com.br>
2020-05-04 07:02:13 -0300

    alterando nome da variavel

--- src/main/java/com/challenge/desafio/CalculadorDeClasses.java
+++ src/main/java/com/challenge/desafio/CalculadorDeClasses.java
@@ -12,44 +12,42 @@ public class CalculadorDeClasses implements Calculavel {
 
     @Override
     public BigDecimal somar(Object obj) {
-        return retornaTotalDaClasse(obj,Somar.class);
+        return retornaTotalDaClasse(obj, Somar.class);
     }
 
     @Override
     public BigDecimal subtrair(Object obj) {
-        return retornaTotalDaClasse(obj,Subtrair.class);
+        return retornaTotalDaClasse(obj, Subtrair.class);
     }
 
     @Override
     public BigDecimal totalizar(Object obj) {
-        BigDecimal somar = somar(obj);
-        BigDecimal subtrair = subtrair(obj);
-        return somar.subtract(subtrair);
+        return somar(obj).subtract(subtrair(obj));
     }
 
     private BigDecimal calcularPontos(Object obj, Class annotation, boolean sup) {
         Class cl;
-        BigDecimal bd = BigDecimal.ZERO;
+        BigDecimal result = BigDecimal.ZERO;
         if (sup) cl = obj.getClass().getSuperclass();
         else cl = obj.getClass();
         for (Field f : cl.getDeclaredFields()) {
             f.setAccessible(true);
             if (f.isAnnotationPresent(annotation) && f.getType().equals(BigDecimal.class)) {
                 try {
-                    bd = bd.add((BigDecimal) f.get(obj));
+                    result = result.add((BigDecimal) f.get(obj));
                 } catch (IllegalAccessException ex) {
                     ex.printStackTrace();
                 }
             }
         }
-        return bd;
+        return result;
     }
 
-    private BigDecimal retornaTotalDaClasse(Object obj,Class annotation){
-        BigDecimal bd = BigDecimal.ZERO;
-        bd = bd.add(calcularPontos(obj, annotation, false));
-        bd = bd.add(calcularPontos(obj,annotation,true));
-        return bd;
+    private BigDecimal retornaTotalDaClasse(Object obj, Class annotation) {
+        BigDecimal result = BigDecimal.ZERO;
+        result = result.add(calcularPontos(obj, annotation, false));
+        result = result.add(calcularPontos(obj, annotation, true));
+        return result;
     }
 
 }
